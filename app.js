99365485c351e8c4056f704bb878f4067c35fcfe
Noni2207 <81325180+Noni2207@users.noreply.github.com>
2021-10-04 18:38:48 +0200

    Update app.js
     Updating the text and color contrast, automatically change when using the sliders

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ let initialColors;
 sliders.forEach(slider => {
    slider.addEventListener("input", hslControls);
 });
+colorDives.forEach((div, index) => {
+    div.addEventListener ("change", () => {
+       updateTextUI(index);
+    });
+});
 
 
 //Function
